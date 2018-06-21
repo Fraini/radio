3ef27fb8a9bbbbc21ceedc52c073761d7654ef5e
Fraini sanchez <jarabaco@gmail.com>
2018-06-21 12:08:00 +0200

    Añado reproductor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StreamingMedia } from '@ionic-native/streaming-media';
+
+
 
 @NgModule({
   declarations: [
@@ -34,6 +37,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    StreamingMedia,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { StreamingMedia, StreamingVideoOptions } from '@ionic-native/streaming-media';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private streamingMedia: StreamingMedia) {
+
+  }
+
+  playAudio(){
+    let urlAudio : string = "http://89.163.231.60:8410/listen,pls?sid=1";
+
+    let options: StreamingVideoOptions = {
+      successCallback: () => { console.log('Video played') },
+      errorCallback: (e) => { console.log('Error streaming') },
+      orientation: 'landscape'
+    };
+
+    this.streamingMedia.playAudio(urlAudio, options);
 
   }
 
