b0972be935c76b67948a8c5ec8512ec6d902d225
Fraini sanchez <jarabaco@gmail.com>
2018-06-21 12:14:54 +0200

    Añado reproductor audio 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { StreamingMedia, StreamingVideoOptions } from '@ionic-native/streaming-media';
+import { StreamingMedia, StreamingAudioOptions } from '@ionic-native/streaming-media';
 
 @Component({
   selector: 'page-home',
@@ -15,11 +15,12 @@ export class HomePage {
   playAudio(){
     let urlAudio : string = "http://89.163.231.60:8410/listen,pls?sid=1";
 
-    let options: StreamingVideoOptions = {
-      successCallback: () => { console.log('Video played') },
-      errorCallback: (e) => { console.log('Error streaming') },
-      orientation: 'landscape'
+    let options: StreamingAudioOptions = {
+      successCallback: () => { console.log('Finished Audio') },
+      errorCallback: (e) => { console.log('Error: ', e) },
+      initFullscreen: false // iOS only!
     };
+
 
     this.streamingMedia.playAudio(urlAudio, options);
 
