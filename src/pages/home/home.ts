c2f508ff505b2afba15530b8c914dac6f642f967
Fraini sanchez <jarabaco@gmail.com>
2018-07-24 19:41:34 +0200

    v4

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,23 +7,48 @@ import { StreamingMedia, StreamingAudioOptions } from '@ionic-native/streaming-m
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  url:string;
+	stream:any;
+  promise:any;
+  volumen:number;
+  saturation : any;
+  stopPause:boolean;
   constructor(public navCtrl: NavController,private streamingMedia: StreamingMedia) {
-
+    this.url = "http://89.163.231.60:8410/listen,pls?sid=1";
+    this.stream = new Audio(this.url);
+    this.volumen = 30;//Esto va de 0 a 100 en el range, pero la propiedad volumen del objeto
+    //va de 0 a 1. ==> Ej Un 30 será 0.3.
+    this.stopPause = true;
   }
 
-  playAudio(){
-    let urlAudio : string = "http://89.163.231.60:8410/listen,pls?sid=1";
+  play() {
+    this.stream.play();
+		this.promise = new Promise((resolve,reject) => {
+			this.stream.addEventListener('playing', () => {
+        resolve(true);
+        this.stopPause = false;
+			});
 
-    let options: StreamingAudioOptions = {
-      successCallback: () => { console.log('Finished Audio') },
-      errorCallback: (e) => { console.log('Error: ', e) },
-      initFullscreen: false // iOS only!
-    };
+			this.stream.addEventListener('error', () => {
+				reject(false);
+			});
+		});
+		return this.promise;
+	};
 
-
-    this.streamingMedia.playAudio(urlAudio, options);
-
+	pause() {
+    this.stopPause = true;
+		this.stream.pause();
+  };
+  
+  vol(vol:number){
+    
+    this.stream.volume = vol;
+  }
+ 
+  actualizar(){
+    let vol = Math.round((this.volumen * 100) / 100)
+    this.vol(vol / 100); 
   }
 
 }
